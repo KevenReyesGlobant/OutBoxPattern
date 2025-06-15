dddbcfae8f43da5f61d9f5673959dc2d09caa0a3
Keven.ReyesGlobant <keven.reyes@globant.com>
2025-06-15 13:44:21 -0500

    Create files from commons

---
+++ customer-service/src/test/java/com/food/ordering/system/customer_service/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food.ordering.system.customer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
