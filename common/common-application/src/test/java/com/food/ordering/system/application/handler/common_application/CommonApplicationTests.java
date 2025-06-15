29fe198cd54aae08c8f26f862840a7464705afe7
Keven.ReyesGlobant <keven.reyes@globant.com>
2025-06-15 13:26:27 -0500

    Initial commit created from manage microservices orders

---
+++ common/common-application/src/test/java/com/food/ordering/system/application/handler/common_application/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food.ordering.system.application.handler.common_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
